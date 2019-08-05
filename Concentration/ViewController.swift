58c21eb354dcffe93c01e40536d0ff6a806f99b0
Emma Walker - TVandMobile Platforms - Core Engineering <walkee28@mc-n358038.local>
2019-08-05 13:24:08 +0100

    Lecture 2 - 1:00:00 controller flips cards but model doesn't match yet

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -69,15 +69,44 @@ class ViewController: UIViewController
                 button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
             } else {
                 button.setTitle("", for: UIControl.State.normal)
+                
+                // this sets the background clear if the card is facedown and is matched
+                
                 button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 0) : #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)
             }
         }
     }
         
-        var emojiChoices = ["👻", "🎃", "🦇" ,"😈"]
+    var emojiChoices = ["👻", "🎃", "🦇" ,"😈", "🤡", "☠️", "🧙‍♀️", "👹"]
 
-        func emoji(for card: Card) -> String {
-            return "?"
+    //we are going to use a dictionary for this. a dictionary is a data structure where you can look something up and retireve a value
+    //here we have defined a dictionary. You specify the key (int) and the value (string):
+    
+    var emoji = [Int:String]()
+    
+    func emoji(for card: Card) -> String {
+        
+        // we are going to populate our dictionary with emojis as they are called for
+        //arc4random_uniform is a pseudo random number generator from zero up to the upper bound 32 bit unsigned integer (UInt32) not inclusive of the upper bound number
+        //however the array's count is an int not an unsigned int
+        //swift does not do type conversion! you need toi explicitly convert types
+        //to convert you need to create a new thing and initlaise it with the value and type you require
+        //UInt32 is a struct with an initialiser that takes an int
+        //but randomIndex is an unsigned int nit an int!! so also need to convert whole thing
+        // here are two ifs together - if the index is nil and there are emojis left in the array, choose an emji, remove it from the array and put in dictionaryß
+        
+            if emoji[card.identifier] == nil , emojiChoices.count > 0 {
+                let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+                emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+            }
+            
+        //Note looking something up in a dictionary returns an optional - this is because what we have looked up may not be there
+        // we could use if let here to deal with the optional but instead we are going to check if the result of looking up the dictionary is nil before we return anything
+        //below is a special operator instead of using if else because it's so commonly used
+        // This says return  emoji[card.identifier] but if it’s nil return "?":
+        
+        return emoji[card.identifier] ?? "?"
+        
         }
     }
 
